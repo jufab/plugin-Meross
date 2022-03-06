46c20e6cd1e8c8847f1dc82bd1d14b931de1d800
Julien Fabre <julien.fabre.33@gmail.com>
2022-03-06 03:30:56 +0100

    fix: utilisation nouvelle dependance meross_iot

--- 3rdparty/meross.py
+++ 3rdparty/meross.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
 
-import os
-import time
-from random import randint
-
-from meross_iot.cloud.devices.door_openers import GenericGarageDoorOpener
-from meross_iot.cloud.devices.hubs import GenericHub
-from meross_iot.cloud.devices.light_bulbs import GenericBulb
-from meross_iot.cloud.devices.power_plugs import GenericPlug
-from meross_iot.cloud.devices.subdevices.thermostats import ValveSubDevice, ThermostatV3Mode
-from meross_iot.manager import MerossManager
-from meross_iot.meross_event import MerossEventType
-from datetime import datetime, timedelta
 import argparse
-import pickle
+import asyncio
 import json
-import pprint
 import os
+import pprint
 import sys
-debug = False
+from datetime import datetime, timedelta
 
+from meross_iot.http_api import MerossHttpClient
+from meross_iot.manager import MerossManager
+from meross_iot.model.enums import OnlineStatus, Namespace
+from meross_iot.model.http.exception import BadLoginException
+
+debug = False
 
 if sys.version_info[0] < 3:
     raise Exception("Must be using Python 3")
@@ -38,6 +32,7 @@ var_dir = current_dir
 # data files
 conffile = os.path.join(var_dir, 'config.ini')
 
+
 # ---------------------------------------------------------------------
 
 
@@ -50,10 +45,11 @@ class WriteLog:
             print(txt)
         return
 
+
 # ---------------------------------------------------------------------
 
 
-def ReadConfig(conffile=conffile):
+def read_config(conffile=conffile):
     """ Read config (secrets) """
     import configparser
     config = configparser.ConfigParser()
@@ -75,32 +71,30 @@ password = your-meross-password
 
 
 # ---------------------------------------------------------------------
-def Exit(txt=""):
+def exit(txt=""):
     print(txt)
     sys.exit(1)
 
 
 # ---------------------------------------------------------------------
-def RefreshOneDevice(device):
+async def refresh_one_device(device):
     """ Connect to Meross Cloud and refresh only the device 'device' """
-
-    if device.online is True or str(device.online) == "1":
-        data = device.get_sys_data()
-        if debug:
-            pprint.pprint(data)
-
+    await device.async_update()
     d = dict({
-        'name':     device.name,
-        'uuid':     device.uuid,
-        'ip':       '',
-        'mac':      '',
-        'online':   device.online,
-        'type':     device.type,
-        'firmversion': device.fwversion,
-        'hardversion': device.hwversion,
+        'name': device.name,
+        'uuid': device.uuid,
+        'ip': '',
+        'mac': '',
+        'online': device.online_status == OnlineStatus.ONLINE,
+        'type': device.type,
+        'firmversion': device.firmware_version,
+        'hardversion': device.hardware_version,
     })
 
-    if device.online is True or str(device.online) == "1":
+    if device.online_status == OnlineStatus.ONLINE:
+        data = device.abilities
+        if debug:
+            pprint.pprint(data)
         try:
             d['ip'] = data['all']['system']['firmware']['innerIp']
         except:
@@ -114,104 +108,102 @@ def RefreshOneDevice(device):
         # on/off status
         onoff = []
         try:
-            onoff = [data['all']['control']['toggle']['onoff']]
+            onoff = device.is_on()
         except:
-            try:
-                ll = data['all']['digest']['togglex']
-                onoff = [x['onoff'] for x in ll]
-            except:
-                pass
+            pass
         d['onoff'] = onoff
 
         # Current power
         try:
-            electricity = device.get_electricity()
-            d['power'] = float(electricity['power'] / 1000.)
+            electricity = await device.async_get_instant_metrics()
+            d['power'] = electricity.power
+            d['voltage'] = electricity.voltage
+            d['current'] = electricity.current
         except:
-            d['power'] = '-1'
+            d['power'] = -1
+            d['voltage'] = -1
+            d['current'] = -1
 
         # Historical consumption
-        #print (dir(device) )
-        d['consumption'] = []   # on decide de ne pas la stocker
+        # print (dir(device) )
+        d['consumption'] = []  # on decide de ne pas la stocker
         try:
-            #pprint.pprint (device.get_power_consumption())
-            l_consumption = device.get_power_consumption()
-
+            # pprint.pprint (device.get_power_consumption())
+            l_consumption = await device.async_get_daily_power_consumption()
             # Yesterday consumption
             today = datetime.today()
             yesterday = (today - timedelta(1)).strftime("%Y-%m-%d")
             d['consumption_yesterday'] = 0
 
             for c in l_consumption:
-                if c['date'] == yesterday:
+                dateconso = c['date'].strftime("%Y-%m-%d")
+                if dateconso == yesterday:
                     try:
-                        d['consumption_yesterday'] = float(c['value'] / 1000.)
+                        d['consumption_yesterday'] = c['total_consumption_kwh']
                     except:
                         d['consumption_yesterday'] = 0
                     break
 
         except:
             d['consumption_yesterday'] = 0
-            #l_consumption = []
-
+            # l_consumption = []
 
     return d
 
+
 # ---------------------------------------------------------------------
 # def event_handler(eventobj):
-    # TODO: handle events in plugin
-    #
-    # if eventobj.event_type == MerossEventType.DEVICE_ONLINE_STATUS:
-    #     # print("Device online status changed: %s went %s" % (eventobj.device.name, eventobj.status))
-    #     pass
-    #
-    # elif eventobj.event_type == MerossEventType.DEVICE_SWITCH_STATUS:
-    #     # print("Switch state changed: Device %s (channel %d) went %s" % (eventobj.device.name, eventobj.channel_id, eventobj.switch_state))
-    # elif eventobj.event_type == MerossEventType.CLIENT_CONNECTION:
-    #     # print("MQTT connection state changed: client went %s" % eventobj.status)
-    #
-    #     # TODO: Give example of reconnection?
-    #
-    # elif eventobj.event_type == MerossEventType.GARAGE_DOOR_STATUS:
-    #     # print("Garage door is now %s" % eventobj.door_state)
-    #
-    # elif eventobj.event_type == MerossEventType.THERMOSTAT_MODE_CHANGE:
-    #     # print("Thermostat %s has changed mode to %s" % (eventobj.device.name, eventobj.mode))
-    #
-    # elif eventobj.event_type == MerossEventType.THERMOSTAT_TEMPERATURE_CHANGE:
-    #     # print("Thermostat %s has revealed a temperature change: %s" % (eventobj.device.name, eventobj.temperature))
-    #
-    # else:
-        # print("Unknown event!")
+# TODO: handle events in plugin
+#
+# if eventobj.event_type == MerossEventType.DEVICE_ONLINE_STATUS:
+#     # print("Device online status changed: %s went %s" % (eventobj.device.name, eventobj.status))
+#     pass
+#
+# elif eventobj.event_type == MerossEventType.DEVICE_SWITCH_STATUS:
+#     # print("Switch state changed: Device %s (channel %d) went %s" % (eventobj.device.name, eventobj.channel_id, eventobj.switch_state))
+# elif eventobj.event_type == MerossEventType.CLIENT_CONNECTION:
+#     # print("MQTT connection state changed: client went %s" % eventobj.status)
+#
+#     # TODO: Give example of reconnection?
+#
+# elif eventobj.event_type == MerossEventType.GARAGE_DOOR_STATUS:
+#     # print("Garage door is now %s" % eventobj.door_state)
+#
+# elif eventobj.event_type == MerossEventType.THERMOSTAT_MODE_CHANGE:
+#     # print("Thermostat %s has changed mode to %s" % (eventobj.device.name, eventobj.mode))
+#
+# elif eventobj.event_type == MerossEventType.THERMOSTAT_TEMPERATURE_CHANGE:
+#     # print("Thermostat %s has revealed a temperature change: %s" % (eventobj.device.name, eventobj.temperature))
+#
+# else:
+# print("Unknown event!")
 
 # ---------------------------------------------------------------------
-def ConnectAndRefreshAll(email, password):
+async def connect_and_refresh_all(email, password):
     """ Connect to Meross Cloud and refresh all devices and informations """
-
     try:
         # Initiates the Meross Cloud Manager. This is in charge of handling the communication with the remote endpoint
-        manager = MerossManager(meross_email=email, meross_password=password)
-
-        # Register event handlers for the manager...
-        # manager.register_event_handler(event_handler)
-
-        # Starts the manager
-        manager.start()
-    except:
-        Exit("<F> Error : can't connect to Meross Cloud ! Please verify Internet connection, email and password !")
-
+        http_api_client = await MerossHttpClient.async_from_user_password(email=email, password=password)
+        # Setup and start the device manager
+        manager = MerossManager(http_client=http_api_client)
+    except BadLoginException:
+        exit(f"<F> Error : can't connect to Meross Cloud ! Please verify Internet connection, email and password !")
+    except BaseException as base:
+        exit(f"Error : {base}")
     # Retrieves the list of supported devices
-    devices = manager.get_supported_devices()
+    await manager.async_init()
+    # Discover devices.
+    await manager.async_device_discovery()
+    devices = manager.find_devices()
 
     # Build dict of devices informations
     d_devices = {}
 
     for num in range(len(devices)):
         if debug:
-            print(50*'=' + '\nnum=', num)
+            print(50 * '=' + '\nnum=', num)
         device = devices[num]
-
-        d = RefreshOneDevice(device=device)
+        d = await refresh_one_device(device=device)
 
         uuid = device.uuid
         d_devices[uuid] = d
@@ -219,88 +211,88 @@ def ConnectAndRefreshAll(email, password):
     if debug:
         pprint.pprint(d_devices)
 
-    manager.stop()
+    # Close the manager and logout from http_api
+    manager.close()
+    await http_api_client.async_logout()
 
     return d_devices
 
+
 # ---------------------------------------------------------------------
 
 
-def ConnectAndSetOnOff(devices, email, password, name=None, uuid=None, mac=None, action='on', channel=0):
+async def connect_and_set_on_off(devices, email, password, name=None, uuid=None, mac=None, action='on', channel=0):
     """ Connect to Meross Cloud and set on or off a smartplug """
-
     if mac and not name and not uuid:
-        Exit("<F> Error : not implemented !")
+        exit("<F> Error : not implemented !")
     if not name and not uuid and not mac:
-        Exit("<F> Error : need at least 'name', 'uuid' or 'mac' parameter to set on or off a smartplug !")
+        exit("<F> Error : need at least 'name', 'uuid' or 'mac' parameter to set on or off a smartplug !")
 
+    """ Connect to Meross Cloud and refresh all devices and informations """
     try:
         # Initiates the Meross Cloud Manager. This is in charge of handling the communication with the remote endpoint
-        manager = MerossManager(meross_email=email, meross_password=password)
-
-        # Register event handlers for the manager...
-        # manager.register_event_handler(event_handler)
-
-        # Starts the manager
-        manager.start()
-    except:
-        Exit("<F> Error : can't connect to Meross Cloud ! Please verify Internet connection, email and password !")
-
+        http_api_client = await MerossHttpClient.async_from_user_password(email=email, password=password)
+        # Setup and start the device manager
+        manager = MerossManager(http_client=http_api_client)
+    except BadLoginException:
+        exit(f"<F> Error : can't connect to Meross Cloud ! Please verify Internet connection, email and password !")
+    except BaseException as base:
+        exit(f"Error : {base}")
+    # Retrieves the list of supported devices
+    await manager.async_init()
+    # Discover devices.
+    await manager.async_device_discovery()
     device = None
-
     if uuid is not None:
-        device = manager.get_device_by_uuid(uuid)
-
+        device = manager.find_devices(device_uuids=uuid)[0]
     elif name is not None:
-        device = manager.get_device_by_name(name)
-
+        device = manager.find_devices(device_name=name)[0]
     if device is not None:
-
-        if device.online:
+        await device.async_update()
+        if device.online_status == OnlineStatus.ONLINE:
             try:
                 if action == 'on':
-                    if channel == 0:
-                        device.turn_on()
-                    else:
-                        device.turn_on_channel(int(channel))
+                    await device.async_turn_on(int(channel))
                 else:
-                    if channel == 0:
-                        device.turn_off()
-                    else:
-                        device.turn_off_channel(int(channel))
+                    await device.async_turn_off(int(channel))
             except:
                 print("Unexpected error:", sys.exc_info()[0])
 
-    devices[device.uuid] = RefreshOneDevice(device)
+    devices[device.uuid] = await refresh_one_device(device)
 
-    manager.stop()
+    # Close the manager and logout from http_api
+    manager.close()
+    await http_api_client.async_logout()
 
     return devices
+
 
 # ---------------------------------------------------------------------
 
 
-def GetByName(d_devices, name):
+def get_by_name(d_devices, name):
     """ Find a Meross Smartplug from name """
     for k in d_devices.keys():
         if (d_devices[k]['name'] == name):
             return d_devices[k]
     return {}
 
+
 # ---------------------------------------------------------------------
 
 
-def GetByUuid(d_devices, uuid):
+def get_by_uuid(d_devices, uuid):
     """ Find a Meross Smartplug from uuid """
     for k in d_devices.keys():
         if (d_devices[k]['uuid'] == uuid):
             return d_devices[k]
     return {}
 
+
 # ---------------------------------------------------------------------
 
 
-def GetByMAC(d_devices, mac):
+def get_by_mac(d_devices, mac):
     """ Find a Meross Smartplug from MAC """
     for k in d_devices.keys():
         if (d_devices[k]['mac'] == mac):
@@ -349,64 +341,65 @@ if __name__ == '__main__':
         # Get from config file (commandline argument)
         elif args.config:
             if not os.isfile(args.config):
-                Exit("<F> Error : can't read '%s' config file!" % args.config)
+                exit("<F> Error : can't read '%s' config file!" % args.config)
             else:
                 try:
-                    email, password = ReadConfig(conffile=args.config)
+                    email, password = read_config(conffile=args.config)
                 except:
-                    Exit("<F> Error : can't read '%s' config file!" % args.config)
+                    exit("<F> Error : can't read '%s' config file!" % args.config)
         # Get from local config file
         elif os.path.isfile(conffile):
             try:
-                email, password = ReadConfig(conffile=conffile)
+                email, password = read_config(conffile=conffile)
             except:
-                Exit("<F> Error : can't read '%s' config file!" % args.config)
+                exit("<F> Error : can't read '%s' config file!" % args.config)
 
         # If not defined --> error
         if not email or not password:
-            Exit("<F> Error : Can't get email and password !")
-
+            exit("<F> Error : Can't get email and password !")
+    d_devices = {}
     # Connect to Meross Cloud and Refresh
-    if args.refresh or refresh:
-        d_devices = ConnectAndRefreshAll(email, password)
-
+    if args.refresh:
+        d_devices = asyncio.run(connect_and_refresh_all(email, password))
     # Set on / off
     if args.set_on:
-        d_devices = ConnectAndSetOnOff(devices=d_devices, email=email, password=password,
-                                       name=args.name, uuid=args.uuid, mac=args.mac, action='on', channel=args.channel)
+        d_devices = asyncio.run(connect_and_set_on_off(devices=d_devices, email=email, password=password,
+                                           name=args.name, uuid=args.uuid, mac=args.mac,
+                                           action='on', channel=args.channel))
     if args.set_off:
-        d_devices = ConnectAndSetOnOff(devices=d_devices, email=email, password=password,
-                                       name=args.name, uuid=args.uuid, mac=args.mac, action='off', channel=args.channel)
+        d_devices = asyncio.run(connect_and_set_on_off(devices=d_devices, email=email, password=password,
+                                           name=args.name, uuid=args.uuid, mac=args.mac,
+                                           action='off', channel=args.channel))
 
     # Find the Smartplug
     SP = None
     if args.name:
         if debug:
             print("<I> Getting informations for Smartplug named '%s' ..." % args.name)
-        SP = GetByName(d_devices=d_devices, name=args.name)
+        SP = get_by_name(d_devices=d_devices, name=args.name)
     elif args.uuid:
         if debug:
             print("<I> Getting informations for Smartplug with uuid '%s' ..." % args.uuid)
-        SP = GetByUuid(d_devices=d_devices, uuid=args.uuid)
+        SP = get_by_uuid(d_devices=d_devices, uuid=args.uuid)
     elif args.mac:
         if debug:
             print("<I> Getting informations for Smartplug with MAC '%s' ..." % args.mac)
-        SP = GetByMAC(d_devices=d_devices, mac=args.mac)
+        SP = get_by_mac(d_devices=d_devices, mac=args.mac)
     else:
         SP = d_devices
 
     # Return only Power value
     if args.show_power:
-        print(str(int(float(SP['power']/1000.))))
+        print(str(int(SP['power'])))
 
     # Return only yesterday Consumption value
     elif args.show_yesterday:
-        print(str(int(float(SP['consumption_yesterday']/1000.))))
+        print(str(int(SP['consumption_yesterday'])))
 
     # Return the JSON output
     elif args.show:
         # pprint.pprint(SP)
-        #jsonarray = json.dumps(SP)
+        # jsonarray = json.dumps(SP)
         if args.name or args.uuid or args.mac:
             d = dict({SP['uuid']: SP})
         else:
